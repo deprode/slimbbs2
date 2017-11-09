b3b45f52460993b8d9bb37007b134edcf4eb9f42
deprode <sensitively+github@gmail.com>
2017-11-09 18:00:26 +0900

    Change setting value from .env

--- src/settings.php
+++ src/settings.php
@@ -18,10 +18,10 @@ return [
 
         'db' => [
             'driver' => 'mysql',
-            'host' => 'localhost',
-            'database' => 'slimbbs',
-            'username' => 'db_user',
-            'password' => 'password',
+            'host' => getenv('MYSQL_HOST'),
+            'database' => getenv('MYSQL_DATABASE'),
+            'username' => getenv('MYSQL_USER'),
+            'password' => getenv('MYSQL_PASSWORD'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
