486e18fb17872f625cdacba4351bd423cd0a4bc7
deprode <sensitively+github@gmail.com>
2017-11-09 17:51:23 +0900

    Create comments table

---
+++ migrations/20171109150403_FirstGenerate.php
@@ -0,0 +1,37 @@
+<?php
+
+use Phpmig\Migration\Migration;
+
+class FirstGenerate extends Migration
+{
+    /**
+     * Do the migration
+     */
+    public function up()
+    {
+        $query = "CREATE TABLE IF NOT EXISTS `comments` (
+          `comment_id` int(11) NOT NULL,
+          `thread_id` int(11) NOT NULL,
+          `user_id` int(11) NOT NULL,
+          `like_count` int(6) NOT NULL DEFAULT 0,
+          `comment` varchar(2000) NOT NULL DEFAULT '',
+          `photo_url` varchar(2000) NOT NULL DEFAULT '',
+          `created_at` datetime NOT NULL,
+          `updated_at` datetime DEFAULT NULL,
+          PRIMARY KEY (`comment_id`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
+        $c = $this->getContainer();
+        $c['db']->query($query);
+    }
+
+    /**
+     * Undo the migration
+     */
+    public function down()
+    {
+        $query = "DROP TABLE IF EXISTS comments";
+
+        $c = $this->getContainer();
+        $c['db']->query($query);
+    }
+}
---
+++ phpmig.php
@@ -0,0 +1,26 @@
+<?php
+
+use \Phpmig\Adapter;
+use Pimple\Container;
+
+$env_file = __DIR__. '/.env';
+if (is_readable($env_file)) {
+    $dot_env = new Dotenv\Dotenv(__DIR__ . '/');
+    $dot_env->load();
+}
+
+$container = new Container();
+
+$container['db'] = function () {
+    echo getenv('MYSQL_DATABASE') . PHP_EOL;
+
+    $dbh = new PDO('mysql:dbname='.getenv('MYSQL_DATABASE').';host='.getenv('MYSQL_HOST'),getenv('MYSQL_USER'),getenv('MYSQL_PASSWORD'));
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $dbh;
+};
+
+$container['phpmig.adapter'] = new Adapter\File\Flat(__DIR__ . DIRECTORY_SEPARATOR . 'migrations/.migrations.log');
+
+$container['phpmig.migrations_path'] = __DIR__ . DIRECTORY_SEPARATOR . 'migrations';
+
+return $container;
