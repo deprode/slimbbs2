975bf07a686382752835d807ba68a7c1b799af26
deprode <sensitively+github@gmail.com>
2017-12-14 23:54:46 +0900

    Add HTTP header
    * クリックジャッキング対策と、XSS対策用のHTTPレスポンスヘッダを設定

--- src/middleware.php
+++ src/middleware.php
@@ -1,4 +1,14 @@
 <?php
 // Application middleware
 
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
 $app->add($container->get('csrf'));
+
+$app->add(function (ServerRequestInterface $request, ResponseInterface $response, callable $next) {
+    $response = $response->withHeader('X-Content-Type-Options', 'nosniff');
+    $response = $response->withHeader('X-Frame-Options', 'SAMEORIGIN');
+
+    return $next($request, $response);
+});
