1704e7ca767bb4cc319e58f7fc76c854a61a905a
deprode <sensitively+github@gmail.com>
2018-02-05 17:46:56 +0900

    Fix to include "like" display in button
    * 「そうだね×N」をボタンに包含するように修正した

--- public/assets/js/like.js
+++ public/assets/js/like.js
@@ -1,14 +1,14 @@
 "use strict";
 
-function likePlus1(id)
-{
+function likePlus1(id) {
     // count++を反映
-    let count = document.getElementById(id + '_like').textContent;
-    document.getElementById(id + '_like').textContent = (parseInt(count) + 1);
+    let count = document.getElementById(id + '--data').dataset.like;
+    document.getElementById(id + '--data').dataset.like = (parseInt(count) + 1);
+    document.getElementById(id + '--submit').value = 'そうだね ×' + (parseInt(count) + 1);
+
 }
 
-function removeLikeButton(id)
-{
+function removeLikeButton(id) {
     document.getElementById(id).querySelector('input[type=submit]').disabled = true;
     document.getElementById(id).disabled = true;
 }
@@ -20,17 +20,17 @@ function addLike(id) {
 
     fetch(add_like_path, {
         method: 'POST',
-        headers: { 'X-Requested-With': 'XMLHttpRequest' },
+        headers: {'X-Requested-With': 'XMLHttpRequest'},
         body: new FormData(document.getElementById(id))
     })
-    .then((response) => {
-        if (!response.ok) {
-            throw Error(response.statusText);
-        }
-        likePlus1(id);
-        removeLikeButton(id);
-    })
-    .catch((e) => {
-        console.error(e);
-    });
+        .then((response) => {
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            likePlus1(id);
+            removeLikeButton(id);
+        })
+        .catch((e) => {
+            console.error(e);
+        });
 }
