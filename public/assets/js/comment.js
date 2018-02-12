8f670a0a071fc545028fd6fd287597b1e7b58b1c
deprode <sensitively+github@gmail.com>
2018-02-12 17:48:40 +0900

    Merge comment script
    * コメントの編集機能とそうだね機能を統合した

--- public/assets/js/comment.js
+++ public/assets/js/comment.js
@@ -3,6 +3,8 @@
 let comments = document.querySelectorAll('section.js-comment');
 Array.from(comments).forEach((comment) => {
     const comment_str = document.getElementById(comment.id).dataset.comment;
+    const like_form = document.getElementById(comment.id).querySelectorAll('form.js-like')[0];
+
     new Vue({
         delimiters: ['${', '}'],
         el: '#' + comment.id,
@@ -10,11 +12,17 @@ Array.from(comments).forEach((comment) => {
             form_id: comment.id,
             comment: comment_str,
             edit: false,
-            pre_comment: comment_str
+            pre_comment: comment_str,
+            like_form_id: like_form.id,
+            count: document.getElementById(like_form.id).dataset.like,
+            unsent: false
         },
         computed: {
             editing: function () {
                 return this.edit;
+            },
+            soudane: function () {
+                return 'そうだね ×' + this.count;
             }
         },
         methods: {
@@ -50,6 +58,40 @@ Array.from(comments).forEach((comment) => {
                     });
 
                 this.pre_comment = this.comment;
+            },
+            //---------
+            // like
+            addLike: function (event) {
+                if (this.unsent) {
+                    return;
+                }
+                const form = new FormData(document.getElementById(this.like_form_id));
+                this.plus1(form);
+            },
+            plus1: function (form) {
+                const _this = this;
+                fetch(add_like_path, {
+                    method: 'POST',
+                    headers: {'X-Requested-With': 'XMLHttpRequest'},
+                    body: form
+                })
+                    .then(function (response) {
+                            if (!response.ok) {
+                                throw Error(response.statusText);
+                            }
+                            _this.updateCount();
+                            _this.disabledButton();
+                        }
+                    )
+                    .catch((e) => {
+                        console.error(e);
+                    });
+            },
+            updateCount: function () {
+                this.count = (parseInt(this.count)) + 1;
+            },
+            disabledButton: function () {
+                this.unsent = true;
             }
         }
     });
