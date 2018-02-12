5e6c930957664a2c13c998bf5844bd6bec2f0ff1
deprode <sensitively+github@gmail.com>
2018-02-12 16:00:41 +0900

    Change like button from vanilla js to Vuejs.
    * そうだねボタンで使うFWをVanilla JSからVueに変更

--- public/assets/js/like.js
+++ public/assets/js/like.js
@@ -1,36 +1,53 @@
 "use strict";
 
-function likePlus1(id) {
-    // count++を反映
-    let count = document.getElementById(id + '--data').dataset.like;
-    document.getElementById(id + '--data').dataset.like = (parseInt(count) + 1);
-    document.getElementById(id + '--submit').value = 'そうだね ×' + (parseInt(count) + 1);
-
-}
-
-function removeLikeButton(id) {
-    document.getElementById(id).querySelector('input[type=submit]').disabled = true;
-    document.getElementById(id).disabled = true;
-}
-
-function addLike(id) {
-    if (document.getElementById(id).disabled) {
-        return;
-    }
-
-    fetch(add_like_path, {
-        method: 'POST',
-        headers: {'X-Requested-With': 'XMLHttpRequest'},
-        body: new FormData(document.getElementById(id))
-    })
-        .then((response) => {
-            if (!response.ok) {
-                throw Error(response.statusText);
+let forms = document.querySelectorAll('form.js-like');
+Array.from(forms).forEach((form) => {
+    new Vue({
+        delimiters: ['${', '}'],
+        el: '#' + form.id,
+        data: {
+            form_id: form.id,
+            count: document.getElementById(form.id).dataset.like,
+            unsent: false
+        },
+        computed: {
+            soudane: function () {
+                return 'そうだね ×' + this.count;
             }
-            likePlus1(id);
-            removeLikeButton(id);
-        })
-        .catch((e) => {
-            console.error(e);
-        });
-}
+        },
+        methods: {
+            addLike: function (event) {
+                if (this.unsent) {
+                    return;
+                }
+                const form = new FormData(document.getElementById(this.form_id));
+                this.plus1(form);
+            },
+            plus1: function (form) {
+                const _this = this;
+                fetch(add_like_path, {
+                    method: 'POST',
+                    headers: {'X-Requested-With': 'XMLHttpRequest'},
+                    body: form
+                })
+                    .then(function (response) {
+                            if (!response.ok) {
+                                throw Error(response.statusText);
+                            }
+                            _this.updateCount();
+                            _this.disabledButton();
+                        }
+                    )
+                    .catch((e) => {
+                        console.error(e);
+                    });
+            },
+            updateCount: function () {
+                this.count = (parseInt(this.count)) + 1;
+            },
+            disabledButton: function () {
+                this.unsent = true;
+            }
+        }
+    });
+});
