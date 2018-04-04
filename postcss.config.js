402733fcb5a5a7302291a1aa131d562a7577ec77
deprode <sensitively+github@gmail.com>
2018-04-04 19:04:13 +0900

    Refactor css
    * baseとstyleを分けた（変換時に統合される）
    * SCSS風のnestingでCSSの保守性が向上した（はず）

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,8 @@ module.exports = (ctx) => ({
     parser: ctx.file.extname === '.sss' ? 'sugarss' : false,
     map: ctx.options.map,
     plugins: [
+        require('postcss-import')(),
+        require('postcss-nested')(),
         require('autoprefixer')({browsers: 'last 2 versions'}),
         require('cssnano')({preset: 'default'})
     ]
